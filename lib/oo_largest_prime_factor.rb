0da2a0103f7a85f01968a9ac7459d55e7231a6a9
dustinlikens <dustinlikens@icloud.com>
2017-01-01 00:37:46 -0800

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,14 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+
+  def initialize(input)
+    @input = input
+  end
+
+  def number
+    @input.prime_division.map(&:first)[-1]
+  end
+
+end
